901f424054d1edc0fb1f30a894b50e7d2520870c
Hashir Baig <hashirbaig@gmail.com>
2019-06-01 16:56:08 +0500

    Implemented functionality to Add university after receiving email

--- app/src/main/java/com/that/edcerts/fragments/AddUniversityFragment.kt
+++ app/src/main/java/com/that/edcerts/fragments/AddUniversityFragment.kt
@@ -3,14 +3,20 @@ package com.that.edcerts.fragments
 import android.annotation.TargetApi
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.that.edcerts.R
 import com.that.edcerts.activities.HomeActivity
 import kotlinx.android.synthetic.main.fragment_add_university.*
+import org.json.JSONObject
 
 class AddUniversityFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -27,8 +33,30 @@ class AddUniversityFragment : Fragment() {
             invitationUrl.setText(url)
         }
 
+        var inputObject = JSONObject()
+        try {
+            inputObject.put(":email", "ljdkjlksj")
+            inputObject.put(":pkey", "shfeiuruewiu")
+        } catch (ex: Exception) {
+
+        }
         buttonAddUniversity.setOnClickListener {
-            startActivity(HomeActivity.newIntent(context))
+
+            var pkey = "lslkdjsakdpodjfslkfsfj"
+            var url = "http://192.168.1.8:3000/UpdatePublicKey/f8dccfcf-b5e8-4aee-aed4-85c77b358448/${edittext_email.text}/${pkey}"
+
+            var jsonObjectRequest = JsonObjectRequest(
+                    Request.Method.PATCH,
+                    url,
+                    inputObject,
+                    Response.Listener {
+                        startActivity(HomeActivity.newIntent(context))
+                    },
+                    Response.ErrorListener {
+                        Log.wtf(TAG, it)
+                    })
+
+            Volley.newRequestQueue(activity).add(jsonObjectRequest)
         }
     }
 
